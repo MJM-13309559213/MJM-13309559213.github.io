4850c55674ba322fd4e0a3afd33f3ab8cdd03d94
Jia Ming <1351849735@qq.com>
2024-01-11 11:53:52 +0800

    Site updated: 2024-01-11 11:53:51

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(105.96855818057804, 36.98247220138091, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(105.970555, 36.977054, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
